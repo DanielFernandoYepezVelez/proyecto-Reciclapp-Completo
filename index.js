80b2827e6f530ea2d1ae43a6f4a9a9767dbef007
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2019-12-01 20:38:00 -0500

    Ultimas Actializaciones desde el servidor

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const { database } = require('./lib/dbConnect');
 /* -------------------------------------------- */
 
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
 
 app.set('port', process.env.PORT || 4000);
 
-app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
